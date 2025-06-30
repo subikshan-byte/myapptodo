591bcbde91d1e3ea024dd8216309cd533645dc92
subikshan <k.subikshandevp@gmail.com>
2025-06-30 16:05:09 +0530

    Update settings.py

--- myapptodo/settings.py
+++ myapptodo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+lu92+gr$%cgd=u%lzsji649#*yx#!)aow%idoogsk9r=m2p3f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
