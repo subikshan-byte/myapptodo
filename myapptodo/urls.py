07d01c8e4a518c52f8f2b2f426ef40acaa7dead7
subikshan <k.subikshandevp@gmail.com>
2025-06-30 14:01:08 +0530

    Update urls.py

--- myapptodo/urls.py
+++ myapptodo/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("todo/",include("todo.urls")),
+    path("myapptodo-4.onrender.com/",include("todo.urls")),
     path('admin/', admin.site.urls),
 ]
