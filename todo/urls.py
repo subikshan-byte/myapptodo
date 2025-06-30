b3519d582453624ce9b04a433a9828184dfb16d4
subikshan <k.subikshandevp@gmail.com>
2025-06-30 13:57:22 +0530

    Update urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name="todo"
 handler404="views.f404"
 urlpatterns = [
-    path("home/",views.login_button,name="login_button"),
+    path("home",views.login_button,name="login_button"),
     path("login",views.login,name='login'),
     path("signup",views.signup,name='signup'),
     path("signupsuccess",views.signup_button,name='signup_button'),
     path("newtask",views.newtask,name="newtask"),
-        path("home/newaskadded",views.newtask_button,name="task_button"),
-        path("",views.index,name="index")
+        path("newaskadded",views.newtask_button,name="task_button"),
+        path("myapptodo-4.onrender.com",views.index,name="index")
 ]
